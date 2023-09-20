556677900b793a5b3a913ae64c67eca1be14f1cc
Stephanie Bowring <stephaniejbowring@gmail.com>
2023-09-20 10:36:36 +0100

    completed mvp, all CRUD functionality working and CSS styling done

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -12,6 +12,18 @@ def show_bucketlist():
     all_cities = City.query.all()
     return render_template('bucketlist.jinja', countries = all_countries, cities = all_cities)
 
+@city_blueprint.route("/bucketlist/visited")
+def visited_destinations():
+    all_countries = Country.query.all()
+    all_cities = City.query.all()
+    return render_template('visited.jinja', countries = all_countries, cities = all_cities)
+
+@city_blueprint.route("/bucketlist/notyetvisited")
+def not_visited_destinations():
+    all_countries = Country.query.all()
+    all_cities = City.query.all()
+    return render_template('not_visited.jinja', countries = all_countries, cities = all_cities)
+
 @city_blueprint.route("/bucketlist/new")
 def add_to_bucketlist():
     all_countries = Country.query.all()
@@ -20,34 +32,52 @@ def add_to_bucketlist():
 
 @city_blueprint.route("/bucketlist/new", methods=["POST"])
 def add_new_city():
-    new_city_name = request.form["city_name"]
-    new_country_id = request.form["countries"]
-    new_city = City(city_name=new_city_name, country_id=new_country_id)
+    city_name = request.form["city_name"]
+    visited = "visited" in request.form
+    # for city.country.country_id:
+    country_id = request.form["country_id"]
+    new_city = City(city_name=city_name, visited = visited, country_id=country_id)
     db.session.add(new_city)
     db.session.commit()
     return redirect ("/bucketlist")
 
-@city_blueprint.route("/bucketlist/<id>/<city_id>")
-def show_one_destination(id, city_id):
-    destination_country_to_show = Country.query.get(id)
-    destination_city_to_show = City.query.get(city_id)    
-    return render_template("one_destination.jinja", country=destination_country_to_show, city=destination_city_to_show)
-
-@city_blueprint.route("/bucketlist/<id>/<city_id>/edit")
-def edit_bucketlist(id, city_id):
-    destination_to_edit = City.query.get(city_id)
-    destination_country = Country.query.get(id)
-    return render_template('update_destination.jinja', city=destination_to_edit, country = destination_country)
-
-@city_blueprint.route("/bucketlist/<id>/<city_id>/edit", methods = ['POST'])
-def update_destination(id, city_id):
-    city_to_update = City.query.get(city_id)
-    city_to_update.country_of_city = Country.query.get(id)
-    city_to_update.country.country_name = request.form["country_name"]
-    city_to_update.city.city_name = request.form["city_name"]
-    city_to_update.city.visited = request.form["visited"]
+@city_blueprint.route("/bucketlist/new/country", methods=["POST"])
+def add_new_country():
+    country_name = request.form["country_name"]
+    new_country = Country(country_name = country_name)
+    db.session.add(new_country)
     db.session.commit()
-    return redirect ("/bucketlist")
+    return redirect ("/bucketlist/new")
+
+@city_blueprint.route("/bucketlist/<int:id>")
+def show_one_destination(id):
+    destination_city_to_show = City.query.get(id)    
+    return render_template("one_destination.jinja", city=destination_city_to_show)
+
+@city_blueprint.route("/bucketlist/<int:id>/edit")
+def edit_bucketlist(id):
+    destination_to_edit = City.query.get(id)
+    return render_template('update_destination.jinja', city=destination_to_edit)
+
+@city_blueprint.route("/bucketlist/<int:id>/edit", methods = ['POST'])
+def update_destination(id):
+    city_to_update = City.query.get(id)
+    city_to_update.country.country_name = request.form["country_name"]
+    city_to_update.city_name = request.form["city_name"]
+    if "visited" in request.form:
+        city_to_update.visited = True
+    else:
+        city_to_update.visited = False
+    db.session.commit()
+    return redirect (f"/bucketlist/{id}")
+
+@city_blueprint.route("/bucketlist/<int:id>/delete", methods= ['POST'])
+def delete_destination(id):
+    city_to_delete = City.query.get(id)
+    db.session.delete(city_to_delete)
+    db.session.commit()
+    return redirect (f"/bucketlist/{id}")
+
 
 
     # chile = Country(country_name = "Chile")
