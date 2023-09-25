1441813410b4aaaec5a36d1502a5182a27111784
John <shetlandjohnmcneill@hotmail.com>
2023-09-25 13:17:54 +0100

    added notes from code r/v

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 from models.country import Country
 from models.city import City
 from app import db
-
+# requests to do with 'countries' should be in this file
 country_blueprint = Blueprint("countries", __name__)
